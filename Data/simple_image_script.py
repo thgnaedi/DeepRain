f787f8c8345868152c3795ebdcc3586e90980c98
Thomas <thgnaedi@htwg-konstanz.de>
2019-03-05 11:50:11 +0100

    basic time logic

--- Data/simple_image_script.py
+++ Data/simple_image_script.py
@@ -6,40 +6,67 @@ import glob
 import re
 
 class Data_converter():
-    def __init__(self, path, max_num_samples, n_data, n_label, start_img=None, sachenzumZuschneiden=None):
+    def __init__(self, path, max_num_samples, n_data, n_label, start_img=None, subimg_startpos=None, subimg_shape=None, output_shape=None):
         assert  os.path.exists(path)
         if start_img is not None:
             assert os.path.exists(start_img)
+            raise NotImplementedError("Path is okay, but functionality not implemented yet!")
         assert n_data > 0 and n_label > 0
         self.path = path
         self.max_num_samples = max_num_samples
         self.n_data = n_data
         self.n_label = n_label
         #ToDo:
-        self.subimg = ((100, 100), (200, 200))
-        self.resize_shape = (60, 60)
+        if subimg_startpos is None or subimg_shape is None:
+            self.subimg = None
+        else:
+            assert isinstance(subimg_startpos, tuple)
+            assert isinstance(subimg_shape, tuple)
+            self.subimg = (subimg_startpos, subimg_shape)#z.B. ((100, 100), (200, 200))
+        assert output_shape is None or isinstance(output_shape, tuple) or isinstance(output_shape, int)
+        self.resize_shape = output_shape#(60, 60)
 
-        self.collect_images()
+        self.all_images = self.collect_images()
         self.create_images()
 
         print("data", self.all_samples[0][0].shape, "label", self.all_samples[0][1].shape)
         return
 
     def collect_images(self):
-        self.all_images = []
+        all_images = []
         path = self.path+"*.png"
         for image_path in glob.glob(path):
-            self.all_images.append(image_path)
-        self.all_images.sort()  # Sortieren
+            all_images.append(image_path)
+        all_images.sort()  # Sortieren
         #ToDo: pattern ersetzen durch regexp
         pattern = "raa01-rw_10000"
-        while len(self.all_images) > 0 and pattern not in self.all_images[0]:
-            del self.all_images[0]
-        while len(self.all_images) > 0 and pattern not in self.all_images[-1]:
-            del self.all_images[-1]
-        #ToDo: jetzt schauen ob Zeitstempel alle passen, sonst fehlerhafte rauslöschen
-        #       Abhängig von n_data und n_label müssen entsprechende anzahl Elemente passen.
-        return
+        while len(all_images) > 0 and pattern not in all_images[0]:
+            del all_images[0]
+        while len(all_images) > 0 and pattern not in all_images[-1]:
+            del all_images[-1]
+
+        all_valid_images = []
+        legals = []
+        first_d = None
+        next_d = None
+        datecomp = Date_Comperator(pre=".\\raa01-rw_10000-",post="-dwd---bin.gz.png")
+        for i in all_images:
+            first_d = next_d
+            next_d = i
+            if first_d is None:
+                legals = [next_d]
+                continue
+            if datecomp.compare(first_d,next_d):
+                # Datum okay:
+                legals.append(next_d)
+                if len(legals) == self.n_data+self.n_label:
+                    all_valid_images.extend(legals)
+                    next_d = None
+                    legals = []
+            else:
+                # Datum nicht okay
+                legals=[next_d]
+        return all_valid_images
 
     def create_images(self):
         min_n = self.n_data+self.n_label
@@ -65,6 +92,26 @@ class Data_converter():
             self.all_samples.append(one_sample)
 
         return
+
+
+class Date_Comperator():
+    def __init__(self, pre, post, timediff=100):
+        self.pre=pre
+        self.post=post
+        self.diff = timediff
+
+    def compare(self, vFirst, vNext):
+        #ToDo: 17:60 gibt es nicht, muss dann zu 1800 umgerechnet werden, überlauf geht weiter usw...
+        time = vFirst.replace(self.pre,"").replace(self.post,"")
+        print(time)
+        time = int(time)
+        time2 = vNext.replace(self.pre,"").replace(self.post,"")
+        print(time2)
+        time2 = int(time2)
+        print(time == time2-self.diff)
+        return time == time2-self.diff
+
+
 
 def open_2D_img(path):
     img = cv2.imread(path)
@@ -174,5 +221,5 @@ def usage():
 
 if __name__ == '__main__':
     path = ".\\"
-    Data_converter(path=path, max_num_samples=2, n_data=2, n_label=1, start_img=None)
-
+    Data_converter(path=path, max_num_samples=2, n_data=1, n_label=1, start_img=None, subimg_startpos=(100,200), subimg_shape=(100,100), output_shape=50)
+    
