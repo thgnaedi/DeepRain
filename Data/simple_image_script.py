192d800babcc5db3a748cea09e0e5ff5ea62979b
Thomas <thgnaedi@htwg-konstanz.de>
2019-03-06 11:43:59 +0100

    [#8 Basics Done, can be used now]remove unnecessary prints, reformat code

--- Data/simple_image_script.py
+++ Data/simple_image_script.py
@@ -5,9 +5,11 @@ import os
 import glob
 import re
 
+
 class Data_converter():
-    def __init__(self, path, max_num_samples, n_data, n_label, start_img=None, subimg_startpos=None, subimg_shape=None, output_shape=None):
-        assert  os.path.exists(path)
+    def __init__(self, path, max_num_samples, n_data, n_label, start_img=None, subimg_startpos=None, subimg_shape=None,
+                 output_shape=None):
+        assert os.path.exists(path)
         if start_img is not None:
             assert os.path.exists(start_img)
             raise NotImplementedError("Path is okay, but functionality not implemented yet!")
@@ -21,14 +23,14 @@ class Data_converter():
         else:
             assert isinstance(subimg_startpos, tuple)
             assert isinstance(subimg_shape, tuple)
-            self.subimg = (subimg_startpos, subimg_shape)#z.B. ((100, 100), (200, 200))
+            self.subimg = (subimg_startpos, subimg_shape)  # z.B. ((100, 100), (200, 200))
         assert output_shape is None or isinstance(output_shape, tuple) or isinstance(output_shape, int)
-        self.resize_shape = output_shape#(60, 60)
+        self.resize_shape = output_shape  # (60, 60)
 
         self.all_images = self.collect_images()
         self.create_images()
 
-        self.id = 0 #id for .get_next()
+        self.id = 0  # id for .get_next()
 
         print("data", self.all_samples[0][0].shape, "label", self.all_samples[0][1].shape)
         return
@@ -47,15 +49,15 @@ class Data_converter():
         return len(self.all_images)
 
     def _info(self):
-        return "DWD Radarbilder"    #ToDo: shape len usw.
+        return "DWD Radarbilder"  # ToDo: shape len usw.
 
     def collect_images(self):
         all_images = []
-        path = self.path+"*.png"
+        path = self.path + "*.png"
         for image_path in glob.glob(path):
             all_images.append(image_path)
         all_images.sort()  # Sortieren
-        #ToDo: pattern ersetzen durch regexp
+        # ToDo: pattern ersetzen durch regexp
         pattern = "raa01-rw_10000"
         while len(all_images) > 0 and pattern not in all_images[0]:
             del all_images[0]
@@ -66,42 +68,43 @@ class Data_converter():
         legals = []
         first_d = None
         next_d = None
-        datecomp = Date_Comperator(pre=self.path+"raa01-rw_10000-",post="-dwd---bin.gz.png")
-        print("habe durchsucht", self.path, "gefunden:", len(all_images))
+        datecomp = Date_Comperator(pre=self.path + "raa01-rw_10000-", post="-dwd---bin.gz.png")
         for i in all_images:
             first_d = next_d
             next_d = i
             if first_d is None:
                 legals = [next_d]
                 continue
-            if datecomp.compare(first_d,next_d):
+            if datecomp.compare(first_d, next_d):
                 # Datum okay:
                 legals.append(next_d)
-                if len(legals) == self.n_data+self.n_label:
+                if len(legals) == self.n_data + self.n_label:
                     all_valid_images.extend(legals)
                     next_d = None
                     legals = []
             else:
                 # Datum nicht okay
-                legals=[next_d]
+                legals = [next_d]
         return all_valid_images
 
     def create_images(self):
-        min_n = self.n_data+self.n_label
+        min_n = self.n_data + self.n_label
         self.all_samples = []
         while len(self.all_images) >= min_n:
             data = None
             label = None
             for i in range(self.n_data):
                 current = self.all_images.pop(0)
-                current = open_one_img(path=current, _subimg=self.subimg, _resize_shape=self.resize_shape, raiseError=True)
+                current = open_one_img(path=current, _subimg=self.subimg, _resize_shape=self.resize_shape,
+                                       raiseError=True)
                 if data is None:
                     data = np.atleast_3d(current)
                 else:
                     data = np.dstack((data, current))
             for i in range(self.n_label):
                 current = self.all_images.pop(0)
-                current = open_one_img(path=current, _subimg=self.subimg, _resize_shape=self.resize_shape, raiseError=True)
+                current = open_one_img(path=current, _subimg=self.subimg, _resize_shape=self.resize_shape,
+                                       raiseError=True)
                 if label is None:
                     label = np.atleast_3d(current)
                 else:
@@ -116,22 +119,21 @@ class Data_converter():
 
 class Date_Comperator():
     def __init__(self, pre, post, timediff=100):
-        print("comperator erstellt mit", pre, " && ", post)
-        self.pre=pre
-        self.post=post
+        self.pre = pre
+        self.post = post
         self.diff = timediff
 
     def compare(self, vFirst, vNext):
-        #ToDo: 17:60 gibt es nicht, muss dann zu 1800 umgerechnet werden, überlauf geht weiter usw...
-        time = vFirst.replace(self.pre,"").replace(self.post,"")
-        print(time)
-        time = int(time)
-        time2 = vNext.replace(self.pre,"").replace(self.post,"")
-        print(time2)
-        time2 = int(time2)
-        print(time == time2-self.diff)
-        return time == time2-self.diff
-
+        # ToDo: 17:60 gibt es nicht, muss dann zu 1800 umgerechnet werden, überlauf geht weiter usw...
+        try:
+            time = vFirst.replace(self.pre, "").replace(self.post, "")
+            time = int(time)
+            time2 = vNext.replace(self.pre, "").replace(self.post, "")
+            time2 = int(time2)
+            return time == time2 - self.diff
+        except:
+            raise ValueError("There are problems converting img name to timestamp!")
+            return False
 
 
 def open_2D_img(path):
@@ -141,7 +143,7 @@ def open_2D_img(path):
         return None
     if len(img.shape) > 2:
         print("Eingelesenes Bild hat zu hohe Dimension (wird gekürzt auf 2D)")
-        img = img[:,:,0]
+        img = img[:, :, 0]
     return img
 
 
@@ -155,12 +157,13 @@ def resize_img(img, shape=(65, 65)):
     if shape is None:
         return img
     if isinstance(shape, int):
-        shape = (shape,shape)
+        shape = (shape, shape)
     assert isinstance(shape, tuple)
     res = cv2.resize(img, dsize=shape, interpolation=cv2.INTER_CUBIC)
     return res
 
-def select_subimg(img, startpos=(0,0), _size=None, raiseError=False):
+
+def select_subimg(img, startpos=(0, 0), _size=None, raiseError=False):
     size = _size
     assert isinstance(startpos, tuple)
     tmp = (img.shape[0] - startpos[0], img.shape[1] - startpos[1])
@@ -171,14 +174,14 @@ def select_subimg(img, startpos=(0,0), _size=None, raiseError=False):
         return img
     if size is None:
         size = tmp
-    size = (min(size[0],tmp[0]), min(size[1],tmp[1]))
+    size = (min(size[0], tmp[0]), min(size[1], tmp[1]))
     if raiseError and not size == _size:
-        raise ValueError("selected size not possible with startposition "+str(startpos))
+        raise ValueError("selected size not possible with startposition " + str(startpos))
 
-    return img[startpos[0]:startpos[0]+size[0],startpos[1]:startpos[1]+size[1]]
+    return img[startpos[0]:startpos[0] + size[0], startpos[1]:startpos[1] + size[1]]
 
 
-#ToDo: Methode noch nicht verwendet, später in Objekt ziehen!
+# ToDo: Methode noch nicht verwendet, später in Objekt ziehen!
 def list_to_set(imgList, n_input, n_output):
     x = None
     y = None
@@ -210,7 +213,7 @@ def open_one_img(path, _subimg=None, _resize_shape=None, raiseError=False, show_
     if _resize_shape is None:
         scaled = img2D_sub
     else:
-        scaled = resize_img(img2D_sub,shape=_resize_shape)
+        scaled = resize_img(img2D_sub, shape=_resize_shape)
     if show_result:
         images = [img2D, img2D_sub, scaled]
         titles = ["Original Bild", "Ausschnitt (200x200)", "Skaliert (80x80)"]
@@ -243,5 +246,5 @@ def usage():
 if __name__ == '__main__':
     path = ".\\"
     path = "C:\\temp\\loeschen\\"
-    Data_converter(path=path, max_num_samples=2, n_data=1, n_label=1, start_img=None, subimg_startpos=(100,200), subimg_shape=(100,100), output_shape=50)
-
+    Data_converter(path=path, max_num_samples=2, n_data=1, n_label=1, start_img=None, subimg_startpos=(100, 200),
+                   subimg_shape=(100, 100), output_shape=50)
