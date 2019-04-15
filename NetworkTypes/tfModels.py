107d09fadcf63b47e77e75986304f9ba33305ee5
Thomas <thgnaedi@htwg-konstanz.de>
2019-04-15 14:53:50 +0200

    new Network, one layer Deeper (next step to U-Net) #14

--- NetworkTypes/tfModels.py
+++ NetworkTypes/tfModels.py
@@ -24,5 +24,36 @@ def network_differentWay(input_shape, use_b_norm=True):
     model.compile(loss="mean_squared_error", optimizer='adam')
     return model
 
+def sameshape_CNN(input_shape, use_b_norm=True):
+    inputs = Input(shape=input_shape)
+
+    conv01 = Conv2D(32, kernel_size=(5, 5), input_shape=input_shape)(inputs)
+    if use_b_norm:
+        conv01 = BatchNormalization()(conv01)
+    conv01 = Activation('relu')(conv01)
+    conv01_pool = MaxPooling2D((3, 3), strides=(3,3))(conv01)
+
+    conv02 = Conv2D(64, kernel_size=(3,3), padding="same")(conv01_pool)
+    if use_b_norm:
+        conv02 = BatchNormalization()(conv02)
+    conv02 = Activation('relu')(conv02)
+    conv02_pool = MaxPooling2D((2, 2), strides=(2,2))(conv02)
+
+    up02 = UpSampling2D((2, 2))(conv02_pool)
+    up02 = concatenate([conv02, up02], axis=3)
+
+
+    up01 = UpSampling2D((3, 3))(up02)
+    if use_b_norm:
+        up01 = BatchNormalization()(up01)
+    up01 = concatenate([conv01, up01], axis=3)
+
+    output = Conv2D(1, (1, 1), activation='relu')(up01)
+    output = Flatten()(output)
+    model = Model(inputs=inputs, outputs=output)
+    model.compile(loss="mean_squared_error", optimizer='adam')
+    #possible losses are: https://www.tensorflow.org/api_docs/python/tf/losses
+    return model
+
 if __name__ == '__main__':
     print("=)")
