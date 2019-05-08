075ff2c891586aa86c2dfdca727f135cef114581
Thomas <thgnaedi@htwg-konstanz.de>
2019-05-08 09:09:12 +0200

    simple UNet for 64x64x5 input images

--- NetworkTypes/tfModels.py
+++ NetworkTypes/tfModels.py
@@ -3,6 +3,54 @@ from tensorflow.python.keras.layers import (Input, Lambda, Conv2D, MaxPooling2D,
                                             Lambda, Activation, BatchNormalization, concatenate, UpSampling2D,
                                             ZeroPadding2D)
 
+def UNet64(input_shape):
+    inputs = Input(shape=input_shape)
+
+    conv01 = Conv2D(10, kernel_size=(3,3), padding="same")(inputs)  #10 x 64x64
+    conv01 = Activation('relu')(conv01)
+    conv01_pool = MaxPooling2D((2, 2), strides=(2, 2))(conv01)      #10 x 32x32
+    print("0)", conv01_pool.shape, "10 x 32x32")
+
+    conv02 = Conv2D(20, kernel_size=(3,3), padding="same")(conv01_pool) #20 x 32x32
+    conv02 = Activation('relu')(conv02)
+    conv02_pool = MaxPooling2D((2, 2), strides=(2, 2))(conv02)          #20 x 16x16
+    print("1)", conv02_pool.shape, "20 x 16x16")
+
+    conv03 = Conv2D(20, kernel_size=(3, 3), padding="same")(conv02_pool)#20 x 16x16
+    conv03 = Activation('relu')(conv03)
+    conv03_pool = MaxPooling2D((2, 2), strides=(2, 2))(conv03)          #20 x 8x8
+    print("2)", conv03_pool.shape, "20 x 8x8")
+
+    conv04 = Conv2D(20, kernel_size=(3, 3), padding="same")(conv03_pool)#20 x 8x8
+    conv04 = Activation('relu')(conv04)
+    conv04_pool = MaxPooling2D((2, 2), strides=(2, 2))(conv04)          #20 x 4x4
+    print("3)", conv04_pool.shape, "20 x 4x4")
+
+### UPSAMPLING:
+    up04 = UpSampling2D((2, 2))(conv04_pool)                            #20 x 8x8
+    up04 = concatenate([conv04, up04], axis=3)                          #20+20 x 8x8
+    print("4)", up04.shape, "40 x 8x8")
+
+    up03 = UpSampling2D((2, 2))(up04)                                   #40 x 16x16
+    up03 = concatenate([conv03, up03], axis=3)                          #20+40 x 16x16
+    print("5)", up03.shape, "60 x 16x16")
+
+    up02 = UpSampling2D((2, 2))(up03)                                   #60 x 32x32
+    up02 = concatenate([conv02, up02], axis=3)                          #20+60 x 32x32
+    print("6)", up02.shape, "80 x 32x32")
+
+    up01 = UpSampling2D((2, 2))(up02)                                   #80 x 64x64
+    up01 = concatenate([conv01, up01], axis=3)                          #10+80 x 64x64
+    print("7)", up01.shape, "90 x 64x64")
+
+    output = Conv2D(1, (1, 1), activation='relu')(up01)                 #1 x 64x64
+    print("8)", output.shape, "1 x 64x64")
+    output = Flatten()(output)
+    model = Model(inputs=inputs, outputs=output)
+    model.compile(loss="mean_squared_error", optimizer='adam')
+    return model
+
+
 def network_differentWay(input_shape, use_b_norm=True):
     inputs = Input(shape=input_shape)
 
