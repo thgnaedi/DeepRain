6350e95d1844759227e0c13a08c1563f5b0bfd4a
Thomas <thgnaedi@htwg-konstanz.de>
2019-06-05 16:25:40 +0200

    parameter to set custom loss function

--- NetworkTypes/tfModels.py
+++ NetworkTypes/tfModels.py
@@ -3,7 +3,7 @@ from tensorflow.python.keras.layers import (Input, Lambda, Conv2D, MaxPooling2D,
                                             Lambda, Activation, BatchNormalization, concatenate, UpSampling2D,
                                             ZeroPadding2D)
 
-def UNet64(input_shape):
+def UNet64(input_shape, lossfunction="mean_squared_error"):
     inputs = Input(shape=input_shape)
 
     conv01 = Conv2D(10, kernel_size=(3,3), padding="same")(inputs)  #10 x 64x64
@@ -47,7 +47,7 @@ def UNet64(input_shape):
     print("8)", output.shape, "1 x 64x64")
     output = Flatten()(output)
     model = Model(inputs=inputs, outputs=output)
-    model.compile(loss="mean_squared_error", optimizer='adam')
+    model.compile(loss=lossfunction, optimizer='adam')
     return model
 
 
