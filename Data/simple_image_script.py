6f252c2a6904f36c72e74f2e47dd9fb55d376f34
Thomas <thgnaedi@htwg-konstanz.de>
2019-06-11 12:58:40 +0200

    degubinformations

--- Data/simple_image_script.py
+++ Data/simple_image_script.py
@@ -105,7 +105,8 @@ class Data_converter():
         min_n = self.n_data + self.n_label
         self.all_samples = []
         number_done = 0
-        number_todo = min(len(self.all_samples)/min_n, self.max_num_samples)
+        print("creating images:", int(len(self.all_samples)/min_n), self.max_num_samples)
+        number_todo = int(len(self.all_samples)/min_n)
         while len(self.all_images) >= min_n:
             data = None
             label = None
