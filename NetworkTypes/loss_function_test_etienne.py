9864ddcebb7d51de961f126c43abab96e357eec0
Etienne Gramlich <etgramlich@gmx.de>
2019-07-24 17:24:42 +0200

    Use all years for training

--- NetworkTypes/loss_function_test_etienne.py
+++ NetworkTypes/loss_function_test_etienne.py
@@ -2,18 +2,20 @@ import NetworkTypes.tfModels as tfM
 import sys
 sys.path.append("../Data")
 sys.path.append("../NetworkTypes")
-import Data.sample_bundle as sampleBundle
+import Data.sample_bundle as sample_bundle
 from NetworkTypes.extendet_CNN_test import train_realdata
+import Final_Networks.predict35minutes
 import numpy as np
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 
 def categorize_data(label):
+    print("Old shape: {}".format(label.shape))
     n_samples = len(label)
     label = label.reshape(n_samples, 64, 64)
-    print("Old shape: {}".format(label.shape))
     new_label_shape = label.shape + (3,)
+    print("New shape: {}".format(new_label_shape))
     labels = np.zeros(new_label_shape, label.dtype)
 
     for idx, value in np.ndenumerate(label):
@@ -28,10 +30,10 @@ def categorize_data(label):
     return labels
 
 
-def main():
+def main(data=None, label=None):
     input_shape = (64, 64, 5)
-    #https://stackoverflow.com/questions/50395170/multi-label-classification-loss-function
-    #https://www.i2tutorials.com/machine-learning-using-tensorflow-tutorial/tensorflow-loss-function/
+    # https://stackoverflow.com/questions/50395170/multi-label-classification-loss-function
+    # https://www.i2tutorials.com/machine-learning-using-tensorflow-tutorial/tensorflow-loss-function/
     model = tfM.UNet64(input_shape,
                        n_predictions=3,
                        lossfunction="categorical_crossentropy",
@@ -39,16 +41,17 @@ def main():
                        activation_output="softmax",
                        metrics=["categorical_accuracy"])
 
-    sb = sampleBundle.load_Sample_Bundle("../Data/RegenTage2016")
+    if data is None:
+        sb = sampleBundle.load_Sample_Bundle("../Data/RegenTage2016")
+        data, label = sb.get_all_data_label(channels_Last=True, flatten_output=True)
 
-    data, label = sb.get_all_data_label(channels_Last=True, flatten_output=True)
     print("Original label shape: {}".format(label.shape))
     label = categorize_data(label)
     n_testsamples = 50
     x_train, y_train = data[n_testsamples:], label[n_testsamples:]
     print("Data shape: {}".format(x_train.shape))
     print("Label shape: {}".format(y_train.shape))
-    #x_test, y_test = data[:n_testsamples], label[:n_testsamples]
+    # x_test, y_test = data[:n_testsamples], label[:n_testsamples]
 
     train_realdata(model=model,
                    samplebundle=None,
@@ -64,4 +67,24 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # Unzip ZIP files to new sub-directory
+    path = "..\\Data\\samplebundles\\unzip\\{}_5in_7out_64x64_without_border"
+    train, test = Final_Networks.predict35minutes.generate_Data_5_7(path)    # 13000 = 4569 18000 = 2904
+
+    # Merge Years to one list
+    all_data = None
+    all_label = None
+    for bundle in train:
+        data_year, label_year = bundle.get_all_data_label(channels_Last=True, flatten_output=False)
+        if all_data is None:
+            all_data = data_year
+            all_label = label_year
+        else:
+            all_data = np.concatenate((all_data, data_year), axis=0)
+            all_label = np.concatenate((all_label, label_year), axis=0)
+    print("collected {} samples for training, 2013 excluded!".format(len(all_data)))
+
+    print("All Data shape: {}".format(all_data.shape))
+    print("All Label shape: {}".format(all_label.shape))
+
+    main(data=all_data, label=all_label[:, :, :, 6])
