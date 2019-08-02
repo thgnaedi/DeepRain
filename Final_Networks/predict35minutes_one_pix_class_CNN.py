8c8d7562349a9a3ee296a47e5de16100e5044db3
Thomas <thgnaedi@htwg-konstanz.de>
2019-08-02 14:45:11 +0200

    added oversampling to increase performance

--- Final_Networks/predict35minutes_one_pix_class_CNN.py
+++ Final_Networks/predict35minutes_one_pix_class_CNN.py
@@ -2,8 +2,9 @@ import numpy as np
 from Final_Networks.predict35minutes_MSE import generate_Data_5_7
 from extendet_CNN_test import train_realdata
 from NetworkTypes.networkBox import get_CNN_classification
+from random import shuffle
 
-def generate_classification(neigbours, print_hist=False, threshold=0.05, only_2004=False):
+def generate_classification(neigbours, print_hist=False, threshold=0.05, only_2004=False, offset=623, duplications=0):
     path = "..\\Data\\samplebundles\\{}_5in_7out_64x64_without_border"
     train, test = generate_Data_5_7(path, only_2004=only_2004)  # 13000 = 4569 18000 = 2904
 
@@ -22,9 +23,11 @@ def generate_classification(neigbours, print_hist=False, threshold=0.05, only_20
     posX, posY = 31, 40  # location kn in image
     flattened_pos = 14174  # flattened id for [31][40][6] #6*64*64+posX*64+posY
     new_label = []
+    new_data = []
     countarray = np.zeros(3)
     countarray2 = np.zeros(3)
     for i in range(len(all_data)):
+        n_duplications=1
         if i == 623:
             countarray2 = np.zeros(3)
         tmp = np.zeros(3)
@@ -33,21 +36,34 @@ def generate_classification(neigbours, print_hist=False, threshold=0.05, only_20
             countarray[0] += 1
             countarray2[0] += 1
         elif all_label[i][flattened_pos] < threshold:
+            if (i >= offset):
+                n_duplications +=duplications
             tmp[1] = 1
-            countarray[1] += 1
-            countarray2[1] += 1
+            countarray[1] += n_duplications
+            countarray2[1] += n_duplications
         else:
+            if (i >= offset):
+                n_duplications +=duplications
             tmp[2] = 1
-            countarray[2] += 1
-            countarray2[2] += 1
-        new_label.append(tmp)
+            countarray[2] += n_duplications
+            countarray2[2] += n_duplications
+
+
+        for j in range(n_duplications):
+            new_label.append(tmp)
+            new_data.append(all_data[i])
+
     new_label = np.array(new_label)
+    new_data = np.array(new_data)
 
     if print_hist:
         print("Klassen Verteilung aller Daten:", countarray)
         print("Klassen Verteilung der trainings Daten:", countarray2)
+        tmp = countarray-countarray2
+        print("trivial solution would be at {}% accuracy".format(tmp[0]*100 / np.sum(tmp)))
 
-    return all_data, new_label
+    return new_data, new_label
+
 
 if __name__ == '__main__':
 
@@ -57,11 +73,9 @@ if __name__ == '__main__':
     model = get_CNN_classification(input_shape=(64, 64, 5))
 
     ### Training vorbereiten:
-    data, label = generate_classification(False)
+    data, label = generate_classification(False, print_hist=True, duplications=2)
     print("sollte sein (8123,3):",label.shape)
-    print(label[123])
-    input("soweit okay?")
 
     ## Training starten:
-    train_realdata(model, samplebundle=None, n_epoch=100, savename="CNN_classification", channelsLast=True, use_logfile=True,
-                   load_last_state=True, n_testsamples=623, data=all_data, label=new_label, _eval_output=False)
+    train_realdata(model, samplebundle=None, n_epoch=15, savename="CNN_classification_2duplications", channelsLast=True, use_logfile=True,
+                   load_last_state=True, n_testsamples=623, data=data, label=label, _eval_output=False)
